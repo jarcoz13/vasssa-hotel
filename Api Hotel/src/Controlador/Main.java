07b5dacc10969025406b76a754f040da440f8c85
lethsphere <lethsphere@gmail.com>
2021-02-23 18:16:02 -0500

    Conexion realizada con exito

--- Api Hotel/src/Controlador/Main.java
+++ Api Hotel/src/Controlador/Main.java
@@ -7,6 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Ventana ventana = new Ventana();
         Control programa = new Control(ventana);
+        //Conexion BD Postgres de Vi
+        //Conexion c = new Conexion();
+        //c.conectarBD("localhost", "5432", "postgres", "postgres", "perseo");
     }
 
 }
